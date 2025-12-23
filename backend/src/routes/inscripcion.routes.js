ebebf85123182b28857d27ef340308ff7f1e592f
BallenaEspartana <luis.campos2201@alumnos.ubiobio.cl>
2025-12-22 21:42:22 -0300

    Se crea la ruta de AlumnosInscritos

--- backend/src/routes/inscripcion.routes.js
+++ backend/src/routes/inscripcion.routes.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware.js";
-import { isAlumno, isJefeCarrera } from "../middleware/role.middleware.js";
+import { isAlumno, isJefeCarrera, isProfesor } from "../middleware/role.middleware.js";
 import {
   handleCreateInscripcion,
-  handleGetInscripciones
+  handleGetInscripciones,
+  handleGetInscripcionesPorElectivo
 } from "../controllers/inscripcion.controller.js";
 
 const router = Router();
 
 router.post("/", [authMiddleware, isAlumno], handleCreateInscripcion);
 router.get("/", [authMiddleware, isJefeCarrera], handleGetInscripciones);
+router.get("/electivo/:electivoId", [authMiddleware, isProfesor], handleGetInscripcionesPorElectivo);
 
 export default router;
